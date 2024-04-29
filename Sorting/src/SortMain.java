c4e89844b78449910d76091aa583468da6e22642
EvanDrake725 <EvanDrake725@gmail.com>
2024-04-29 16:33:42 -0400

    Sorting Upload

---
+++ Sorting/src/SortMain.java
@@ -0,0 +1,8 @@
+public class SortMain {
+    static Sort sort=new Sort();
+    public static void main(String[] args){
+        sort.coolio();
+        sort.unsorted();
+        sort.sorted();
+    }
+}
